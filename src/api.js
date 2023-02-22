a08b2aee784384e7601a083be521448c1f1582ce
marelas <marela.as@gmail.com>
2023-02-22 15:59:03 -0300

    add icons

---
+++ src/api.js
@@ -0,0 +1,17 @@
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '0116475a51msh6a111a11ba25ab8p136c0ejsnfa9b3196f89b',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cbd8003d0cd7c5d9ade209b4a0d50514";
+
+export const IP_ADD_API = "https://ipapi.co/json/";
+
+
