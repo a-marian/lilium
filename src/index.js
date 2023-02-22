6946df01b8329e2f3f7e1c0b5b4ba694d423a09b
marelas <marela.as@gmail.com>
2023-02-22 12:33:36 -0300

    adding git-pages and more code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import App from './App';
-
-const client = new ApolloClient({
-    uri: 'http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={API key}',
-    cache: new InMemoryCache(),
-});
 
 // Supported in React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-);
+
